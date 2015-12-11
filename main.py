d1546eaed0113d418a4bbaa15dedabad3ec3bfea
zachcp <zcharlop@rockefeller.du>
2015-12-10 22:58:58 -0500

    refactor away the utilities file

--- main.py
+++ main.py
@@ -43,12 +43,10 @@ def makecassettes():
     cassetes = GapProcessor(gapdata)
     return json.dumps(cassetes.export())
 
-
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, Nothing at this URL.', 404
-
 
 @app.errorhandler(500)
 def application_error(e):
